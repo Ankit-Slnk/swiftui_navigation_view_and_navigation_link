bc5c9a1048acd1cca449ae9c1d14186cc2613e44
Ankit Solanki <ankit_ms95@yahoo.com>
2021-05-15 15:01:09 +0530

    Initial Commit

---
+++ NavigationView Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationView Demo
+//
+//  Created by Ankit Solanki on 15/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationView Demo/NavigationView_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationView_DemoApp.swift
+//  NavigationView Demo
+//
+//  Created by Ankit Solanki on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationView_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
